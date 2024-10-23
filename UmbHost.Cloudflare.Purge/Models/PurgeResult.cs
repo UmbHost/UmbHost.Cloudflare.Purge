89cd3efa32efe275aad7554fc2819ff534d040cb
Aaron Sadler <aaron@umbhost.net>
2024-10-23 19:57:42 +0100

    Fixed incorrect response type

---
+++ UmbHost.Cloudflare.Purge/Models/PurgeResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UmbHost.Cloudflare.Purge.Models
+{
+    public class PurgeResult
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = string.Empty;
+    }
+}
